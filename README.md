bd54dbf51644ef0bcf7dd4468325e052225cb668
Sambid Rana <sambid.rana1@gmail.com>
2023-04-09 21:56:38 +1000

    Tic-Tac-Toe 2.0 Final

--- README.md
+++ README.md
@@ -1,12 +1,26 @@
 # Project 0 - Tic-Tac-Toe
 
+### Description
 The object of Tic-Tac-Toe is to have three of your symbols make a straight line. The line can go sideways, up and down, or diagonally. The first player to make a straight line with three of their symbols wins! If nobody has a straight line of three symbols when the grid is filled, it's a tie!
+
+### Game Guide
+* Player 1 gets to choose 'X' or 'O' before the game starts.
+* Player 2 will automatically get the remaining choice.
+* Player 1 makes the first move and the game will continue till there is winner or if it ends in a draw.
+* After the first round the player will get the option to play again or end the game.
+* If the players decide to play again, player can decide who goes first and the score counter will keep a track of the wins of both players.
+* If the player decides to end the game the score counter will reset to 0 and the player will be taken to the start game page.
+
+### Technologie/Framework Used
+* HTML
+* CSS
+* JavaScript
+* jQuery
+* Animate.css
 
 ### Link
 A link to the live version of the game: [Tic-Tac-Toe](https://sambidrana.github.io/tic-tac-toe/)
 
 
 ### Acknowledgement
-A special thanks to **CJ**, **Joel**, **Mark**, and **Tina** without whom the project couldn't have been completed. My mom and dad, who are thousands of kilometers away and have no clue as to what I'm doing and to my partner who hasn't complained once fot the past few weeks as I sit in front of the computer day and night. 
-
-*Note*: Remember to delete it before final submission
+A special thanks to **CJ**, **Joel**, **Mark**, and **Tina** for their support and guidance throughout the project.
--- js/bugFix2.0.js
+++ js/bugFix2.0.js
@@ -1,275 +1,293 @@
+$(document).ready(function () {
+    //Selecting the boxes
+    const $box1 = $('#one');
+    const $box2 = $('#two');
+    const $box3 = $('#three');
+    const $box4 = $('#four');
+    const $box5 = $('#five');
+    const $box6 = $('#six');
+    const $box7 = $('#seven');
+    const $box8 = $('#eight');
+    const $box9 = $('#nine');
 
-//Selecting the boxes
-const $box1 = $('#one');
-const $box2 = $('#two');
-const $box3 = $('#three');
-const $box4 = $('#four');
-const $box5 = $('#five');
-const $box6 = $('#six');
-const $box7 = $('#seven');
-const $box8 = $('#eight');
-const $box9 = $('#nine');
+    //buttons choice
+    const $buttonO = $('#O');
+    const $buttonX = $('#X');
 
-//buttons choice
-const $buttonO = $('#O');
-const $buttonX = $('#X');
+    ///////////////////////////////////////////////////////
 
-///////////////////////////////////////////////////////
-
-//active player
-let activePlayer;
-let score = {
-    X: 0,
-    O: 0,
-}
-//switch player
-const switchPlayer = function () {
-    activePlayer = activePlayer === "X" ? "O" : "X";
-};
-
-//click events
-const userSelect = function () {
-
-    $('.box').on('click', function () {
-
-        if ($(this).text() === "X" || $(this).text() === "O") {
-
-            return; //so the user can't click again
-        };
+    //active player
+    let activePlayer;
+    let score = {
+        X: 0,
+        O: 0,
+    }
+    //switch player
+    const switchPlayer = function () {
+        activePlayer = activePlayer === "X" ? "O" : "X";
 
         if (activePlayer === "X") {
-            $(this).addClass('player1');
-            $(this).text(activePlayer);
+            $('#button-text-display').text('X\'s turn')
+        } else (
+            $('#button-text-display').text('O\'s turn')
 
-            switchPlayer();
-            checkWinner();
-            displayWinOrDraw();
-            showPlayAgain();
-            scoreKeeper()
+        )
+    };
 
-        } else if (activePlayer === "O") {
-            $(this).addClass('player2');
-            $(this).text(activePlayer);
+    //click events
+    const userSelect = function () {
 
-            switchPlayer();
-            checkWinner();
-            displayWinOrDraw();
-            showPlayAgain();
-            scoreKeeper()
+        $('.box').on('click', function () {
+
+            if ($(this).text() === "X" || $(this).text() === "O") {
+
+                return; //so the user can't click again
+            };
+
+            if (activePlayer === "X") {
+                $(this).addClass('player1');
+                $(this).text(activePlayer);
+
+                switchPlayer();
+                checkWinner();
+                displayWinOrDraw();
+                showPlayAgain();
+                scoreKeeper()
+
+            } else if (activePlayer === "O") {
+                $(this).addClass('player2');
+                $(this).text(activePlayer);
+
+                switchPlayer();
+                checkWinner();
+                displayWinOrDraw();
+                showPlayAgain();
+                scoreKeeper()
+
+            }
+        }
+        )
+    };
+    userSelect()
+    //winning logic - 8 possible winning combination
+    // [123], [456], [789] - Horizontally
+    // [147], [258], [369] - Vertically
+    // [159], [357]        - Diagonally
+
+    let winner;
+
+    const checkWinner = function () {
+
+        // X conditions
+        if ($box1.text() === "X" && $box2.text() === "X" && $box3.text() === "X") { //Horizontal Win
+
+            return winner = "X";
+
+        } else if ($box4.text() === "X" && $box5.text() === "X" && $box6.text() === "X") {
+
+            return winner = "X";
+
+        } else if ($box7.text() === "X" && $box8.text() === "X" && $box9.text() === "X") {
+
+            return winner = "X";
+
+        } else if ($box1.text() === "X" && $box4.text() === "X" && $box7.text() === "X") { //Vertical Win
+
+            return winner = "X";
+
+        } else if ($box2.text() === "X" && $box5.text() === "X" && $box8.text() === "X") {
+
+            return winner = "X";
+
+        } else if ($box3.text() === "X" && $box6.text() === "X" && $box9.text() === "X") {
+
+            return winner = "X";
+
+        } else if ($box1.text() === "X" && $box5.text() === "X" && $box9.text() === "X") { //Diagonal Win
+
+            return winner = "X";
+
+        } else if ($box3.text() === "X" && $box5.text() === "X" && $box7.text() === "X") {
+
+            return winner = "X";
 
         }
-    }
-    )
-};
-userSelect()
-//winning logic - 8 possible winning combination
-// [123], [456], [789] - Horizontally
-// [147], [258], [369] - Vertically
-// [159], [357]        - Diagonally
 
-let winner;
+        // O conditions
 
-const checkWinner = function () {
+        if ($box1.text() === "O" && $box2.text() === "O" && $box3.text() === "O") { //Horizontal Win
 
-    // X conditions
-    if ($box1.text() === "X" && $box2.text() === "X" && $box3.text() === "X") { //Horizontal Win
+            return winner = "O";
 
-        return winner = "X";
+        } else if ($box4.text() === "O" && $box5.text() === "O" && $box6.text() === "O") {
 
-    } else if ($box4.text() === "X" && $box5.text() === "X" && $box6.text() === "X") {
+            return winner = "O";
 
-        return winner = "X";
+        } else if ($box7.text() === "O" && $box8.text() === "O" && $box9.text() === "O") {
 
-    } else if ($box7.text() === "X" && $box8.text() === "X" && $box9.text() === "X") {
+            return winner = "O";
 
-        return winner = "X";
+        } else if ($box1.text() === "O" && $box4.text() === "O" && $box7.text() === "O") { //Vertical Win
 
-    } else if ($box1.text() === "X" && $box4.text() === "X" && $box7.text() === "X") { //Vertical Win
+            return winner = "O";
 
-        return winner = "X";
+        } else if ($box2.text() === "O" && $box5.text() === "O" && $box8.text() === "O") {
 
-    } else if ($box2.text() === "X" && $box5.text() === "X" && $box8.text() === "X") {
+            return winner = "O";
 
-        return winner = "X";
+        } else if ($box3.text() === "O" && $box6.text() === "O" && $box9.text() === "O") {
 
-    } else if ($box3.text() === "X" && $box6.text() === "X" && $box9.text() === "X") {
+            return winner = "O";
 
-        return winner = "X";
+        } else if ($box1.text() === "O" && $box5.text() === "O" && $box9.text() === "O") { //Diagonal Win
 
-    } else if ($box1.text() === "X" && $box5.text() === "X" && $box9.text() === "X") { //Diagonal Win
+            return winner = "O";
 
-        return winner = "X";
+        } else if ($box3.text() === "O" && $box5.text() === "O" && $box7.text() === "O") {
 
-    } else if ($box3.text() === "X" && $box5.text() === "X" && $box7.text() === "X") {
+            return winner = "O";
 
-        return winner = "X";
+        }
+        return winner;
 
     }
 
-    // O conditions
+    //Display win or draw function
+    let displayResult;
 
-    if ($box1.text() === "O" && $box2.text() === "O" && $box3.text() === "O") { //Horizontal Win
+    const displayWinOrDraw = function () {
+        //if win
+        if (winner) {
+            activePlayer = "";
+            displayResult = $('#display-result').text(`The winner is ${winner}`);
 
-        return winner = "O";
+        }
+        //if draw
+        const boxFilled = $('.box').text();
 
-    } else if ($box4.text() === "O" && $box5.text() === "O" && $box6.text() === "O") {
+        if (boxFilled.split('').length === 9 && !winner) {
+            winner = `DRAW!`;
+            displayResult = $('#display-result').text(`Its a ${winner}`);
 
-        return winner = "O";
+        };
 
-    } else if ($box7.text() === "O" && $box8.text() === "O" && $box9.text() === "O") {
-
-        return winner = "O";
-
-    } else if ($box1.text() === "O" && $box4.text() === "O" && $box7.text() === "O") { //Vertical Win
-
-        return winner = "O";
-
-    } else if ($box2.text() === "O" && $box5.text() === "O" && $box8.text() === "O") {
-
-        return winner = "O";
-
-    } else if ($box3.text() === "O" && $box6.text() === "O" && $box9.text() === "O") {
-
-        return winner = "O";
-
-    } else if ($box1.text() === "O" && $box5.text() === "O" && $box9.text() === "O") { //Diagonal Win
-
-        return winner = "O";
-
-    } else if ($box3.text() === "O" && $box5.text() === "O" && $box7.text() === "O") {
-
-        return winner = "O";
-
-    }
-    return winner;
-
-}
-
-//Display win or draw function
-let displayResult;
-
-const displayWinOrDraw = function () {
-    //if win
-    if (winner) {
-        activePlayer = "";
-        displayResult = $('#display-result').text(`The winner is ${winner}`);
-
-    }
-    //if draw
-    const boxFilled = $('.box').text()
-
-    if (boxFilled.split('').length === 9 && !winner) {
-        winner = `DRAW!`;
-        displayResult = $('#display-result').text(`Its a ${winner}`);
+        return;
 
     };
 
-    return;
+    //Play again button
+    $('#reset-button').on('click', function () {
 
-};
+        activePlayer = "";
+        winner = "";
+        $('.box').text('');
+        $('.box').removeClass('player1');
+        $('.box').removeClass('player2');
+        $('#display-result').text('');
 
-//Play again button
-$('#reset-button').on('click', function () {
 
-    activePlayer = "";
-    winner = "";
-    $('.box').text('');
-    $('.box').removeClass('player1');
-    $('.box').removeClass('player2');
-    $('#display-result').text('');
+        $('#main-container').removeClass('main-visibility');
+        $buttonX.removeClass('main-visibility');
+        $buttonO.removeClass('main-visibility');
 
-    
-    $('#main-container').removeClass('main-visibility');
-    $buttonX.removeClass('main-visibility');
-    $buttonO.removeClass('main-visibility');
+        $buttonO.show();
+        $buttonX.show();
+        $('#reset-button').hide();
+        $('#button-text-display').text('Choose who goes first now? ').show();
+        $('#end-game').hide();
 
-    $buttonO.show();
-    $buttonX.show();
-    $('#reset-button').hide();
-    $('#button-text-display').text('Choose who goes first now? ').show();
-    $('#end-game').hide()
+    }
+    );
 
-}
-);
+    //End game button
+    $('#end-game').on('click', function () {
+        location.reload();
+    })
 
-//End game button
-$('#end-game').on('click', function (e) {
-    location.reload();
-})
+    //Score Keeper 
+    const scoreKeeper = function () {
 
-//Score Keeper 
-const scoreKeeper = function () {
+        if (winner === "X") {
+            score.X += 1;
+            $('#score-X p').text(`${score.X}`)
+        } else if (winner === "O") {
+            score.O += 1;
+            $('#score-O p').text(`${score.O}`)
+        }
 
-    if(winner === "X") {
-        score.X +=1;
-        $('#score-X p').text(`${score.X}`)
-    } else if (winner === "O") {
-        score.O +=1;
-        $('#score-O p').text(`${score.O}`)
+
     }
 
-}
+    //Start game page effect
+    $('#header-button').on('click', function () {
 
-//Start game page effect
-$('#header-button').on('click', function () {
+        $('#header-container').addClass("header-hide");
+        $('#header-container').removeAttr('id');
+        $('#button-hide').attr('id', 'button-container');
+        $('#button-hide').removeAttr('id');
 
-    $('#header-container').addClass("header-hide");
-    $('#header-container').removeAttr('id');
-    $('#button-hide').attr('id', 'button-container')
-    $('#button-hide').removeAttr('id');
+    });
+
+    // O or X selection after start game page
+    if (!activePlayer) {
+
+        $buttonX.on('click', function () {
+
+            activePlayer = $buttonX.text();
+            $buttonO.hide();
+            $buttonX.hide();
+
+            $('#button-text-display').text(`X makes the first move`);
+            $('.main-hide').attr('id', 'main-container');
+            $('.main-hide').removeClass();
+            $('.scoreX-hide, .scoreO-hide').removeClass();
+
+        })
+    };
+    if (!activePlayer) {
+
+        $buttonO.on('click', function () {
+
+            activePlayer = $buttonO.text();
+            $buttonX.hide();
+            $buttonO.hide();
+
+            $('#button-text-display').text(`O makes the first move`);
+            $('.main-hide').attr('id', 'main-container');
+            $('.main-hide').removeClass();
+            $('.reset-hide').attr('id', 'reset-button-container');
+            $('.reset-hide').removeClass();
+            $('.scoreX-hide, .scoreO-hide').removeClass();
+
+        })
+    };
+
+
+
+
+    //Bring restat game up on screen after win/loss or draw
+    const showPlayAgain = function () {
+
+        if (winner && displayResult) {
+
+            $('#main-container').addClass('main-visibility');
+            $buttonX.addClass('main-visibility');
+            $buttonO.addClass('main-visibility');
+
+            $('#display-result').text(`${displayResult.text()}`);
+            $('#display-result').attr('id', 'display-result');
+            $('.reset-hide').attr('id', 'reset-button-container');
+            $('.reset-hide').removeClass(`${displayResult.text()}`);
+            $buttonO.hide();
+            $buttonX.hide();
+            $('#button-text-display').hide();
+            $('#reset-button').show();
+            $('#end-game').show();
+
+        }
+
+        return;
+    };
+
 
 });
-
-// O or X selection after start game page
-if (!activePlayer) {
-
-    $buttonX.on('click', function () {
-
-        activePlayer = $buttonX.text();
-        $buttonO.hide();
-        $('#button-text-display').text(`You selected -`);
-        $('.main-hide').attr('id', 'main-container');
-        $('.main-hide').removeClass();
-        $('.scoreX-hide, .scoreO-hide').removeClass()
-
-    })
-};
-if (!activePlayer) {
-
-    $buttonO.on('click', function () {
-
-        activePlayer = $buttonO.text();
-        $buttonX.hide();
-        $('#button-text-display').text(`You chose - `);
-        $('.main-hide').attr('id', 'main-container');
-        $('.main-hide').removeClass();
-        $('.reset-hide').attr('id', 'reset-button-container');
-        $('.reset-hide').removeClass();
-        $('.scoreX-hide, .scoreO-hide').removeClass()
-
-    })
-};
-
-//Bring restat game up on screen after win/loss or draw
-const showPlayAgain = function () {
-
-    if (winner && displayResult) {
-
-        $('#main-container').addClass('main-visibility');
-        $buttonX.addClass('main-visibility');
-        $buttonO.addClass('main-visibility');
-
-        $('#display-result').text(`${displayResult.text()}`)
-        $('#display-result').attr('id', 'display-result') //
-        $('.reset-hide').attr('id', 'reset-button-container')
-        $('.reset-hide').removeClass(`${displayResult.text()}`);
-        $buttonO.hide();
-        $buttonX.hide();
-        $('#button-text-display').hide();
-        $('#reset-button').show();
-        $('#end-game').show()
-
-    }
-
-    return;
-};
